85253bcebdad67e1867ffd1d065b2071843ae288
Kamran <83241419+NarmakTwo@users.noreply.github.com>
2023-06-15 19:15:51 -0400

    Update script.js

--- client/script.js
+++ client/script.js
@@ -67,11 +67,7 @@ function reset() {
 }
 
 function start() {
-  if (nameBox1.value === "") {
-    player.name = nameBox2.value;
-  }else{
-    player.name = nameBox1.value + " " + nameBox2.value;
-  }
+  player.name = document.getElementById("nameBox").value
   document.getElementById("startBtn").style.display = "none";
   document.getElementById("name1").style.display = "none";
   document.getElementById("name2").style.display = "none";
